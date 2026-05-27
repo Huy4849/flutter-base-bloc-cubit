d5ab479f22c0bb6a78199dc579e1596e28bcdde0
Huy Nguyen <huy4849@gmail.com>
2026-05-27 15:53:13 +0700

    refactor HomeScreen UI to a GridView layout

--- lib/presentation/xelauikit_screens/home_screen.dart
+++ lib/presentation/xelauikit_screens/home_screen.dart
@@ -1,9 +1,7 @@
-import 'package:bnv_opendata/widgets/xela_widgets/xela_color.dart';
-import 'package:bnv_opendata/widgets/xela_widgets/xela_text_style.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -12,60 +10,61 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: const BoxDecoration(color: Colors.white),
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        fit: BoxFit.fitWidth,
-                        image: AssetImage('assets/images/home_bg.png'),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height,
-                padding: const EdgeInsets.fromLTRB(0, 64, 0, 0),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      "assets/images/logo.png",
-                      width: 212,
-                      height: 34,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(4),
-                      child: const Text(
-                        "DESIGN SYSTEM",
-                        style: XelaTextStyle.XelaSmallBodyBold,
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
-                      child: Text(
-                        "Perfectly shaped UI kit for Flutter App with reusable components for your next mobile and desktop apps.\nSaves time and helps teams and indiehackers to release Flutter applications faster.",
-                        textAlign: TextAlign.center,
-                        style: XelaTextStyle.XelaBody.apply(
-                            color: XelaColor.Gray2),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )),
-    );
+    return SafeArea(
+        child: Scaffold(
+            body: GridView.count(
+      crossAxisCount: 2,
+      padding: const EdgeInsets.all(24),
+      mainAxisSpacing: 30,
+      crossAxisSpacing: 30,
+      children: [
+        Container(
+          width: 150,
+          height: 150,
+          color: Colors.red,
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          color: Colors.green,
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          color: Colors.yellow,
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 150,
+          height: 150,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Color(0xFF023F40),
+          ),
+        ),
+        Stack(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.orange),
+            ),
+            Positioned(
+                top: 60,
+                left: 60,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                )),
+          ],
+        )
+      ],
+    )));
   }
 }
